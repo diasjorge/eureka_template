981916027f775216535f9c5eec1f90396e488bc6
Jorge Dias <jorge.dias@schibsted.com>
2015-11-27 17:25:54 +0100

    Add tests for template using collection features

--- tests/test_template.py
+++ tests/test_template.py
@@ -5,7 +5,7 @@ from eureka_template.template import Template
 from eureka_client_mock import EurekaClientMock
 
 
-class TestTemplate(unittest.TestCase):
+class TestTemplateApplications(unittest.TestCase):
 
     def setUp(self):
         eureka_client = EurekaClientMock()
@@ -20,6 +20,21 @@ class TestTemplate(unittest.TestCase):
     def test_render_instances(self):
         for app in self.conf:
             self.assertGreaterEqual(len(app['instances']), 1)
+
+
+class TestTemplateApplication(unittest.TestCase):
+
+    def setUp(self):
+        eureka_client = EurekaClientMock()
+        template_file = os.path.join(os.path.dirname(__file__),
+                                     'fixtures', 'template-app.j2')
+        template = Template(eureka_client, template_file)
+        self.conf = json.loads(template.render())
+
+    def test_render_application(self):
+        self.assertEqual(1, len(self.conf))
+        self.assertEqual("MYAPPLICATION", self.conf[0]["name"])
+
 
 if __name__ == '__main__':
     unittest.main()
