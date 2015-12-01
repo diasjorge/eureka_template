0bc139b335da4465b09b17aa4be573c5da9631ba
Jorge Dias <jorge.dias@schibsted.com>
2015-12-01 11:45:53 +0100

    Preparing release 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='eureka_template',
-    version='0.0.1',
+    version='0.0.2',
     description='Eureka Templating Library',
     author='Jorge Dias',
     author_email='jorge@mrdias.com',
