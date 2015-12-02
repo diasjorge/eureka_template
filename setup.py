a5219f9cae33ed2b11b616d60c703955b3b9fc22
Jorge Dias <jorge.dias@schibsted.com>
2015-12-02 15:20:03 +0100

    Back to development: 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='eureka_template',
-    version='0.0.4',
+    version='0.0.5.dev0',
     description='Eureka Templating Library',
     author='Jorge Dias',
     author_email='jorge@mrdias.com',
